1a20f2122da0b816615ad7e00c39d0131aaf6f4e
Saurabh Gupta <saurabh3949@gmail.com>
2016-01-26 10:57:38 -0800

    Add test cases

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+from Interval import Interval
+from IntervalArray import IntervalArray
+
+class TestCases(unittest.TestCase):
+    """Tests for `solution.py`."""
+
+    def test_add_ranges(self):
+        """Addition cases here"""
+
+
+    def test_delete_ranges(self):
+        """Deletion cases here"""
+
+    def test_query(self):
+        """querying cases here"""
+
+
+if __name__ == '__main__':
+    unittest.main()
